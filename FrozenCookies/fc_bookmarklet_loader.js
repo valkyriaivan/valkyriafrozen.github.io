94b2c14a0ff4acf6175fd83ee7db0ae70ced057c
valkyriaivan <valkyriaivan@yahoo.es>
2022-01-25 11:51:31 +0100

    FrozenCookies

---
+++ FrozenCookies/fc_bookmarklet_loader.js
@@ -0,0 +1,3 @@
+javascript: (function () {
+  Game.LoadMod("https://icehawk78.github.io/FrozenCookies/frozen_cookies.js");
+})();
---
+++ FrozenCookies/fc_userscript_loader.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name           Frozen Cookies
+// @version        github-latest
+// @description    Userscript to load Frozen Cookies
+// @author         Icehawk78
+// @homepage       https://github.com/icehawk78/FrozenCookies
+// @include        http://orteil.dashnet.org/cookieclicker/
+// @include        https://orteil.dashnet.org/cookieclicker/
+// @updateURL      https://icehawk78.github.io/FrozenCookies/fc_userscript_loader.user.js
+// @downloadURL    https://icehawk78.github.io/FrozenCookies/fc_userscript_loader.user.js
+// ==/UserScript==
+
+// Source:    https://github.com/Icehawk78/FrozenCookies/main/
+// Github.io: http://icehawk78.github.io/FrozenCookies/
+
+var loadInterval = setInterval(function () {
+  const Game = unsafeWindow.Game;
+  if (Game && Game.ready) {
+    clearInterval(loadInterval);
+    Game.LoadMod("https://icehawk78.github.io/FrozenCookies/frozen_cookies.js");
+  }
+}, 1000);
